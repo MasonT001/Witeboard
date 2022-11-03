01cd71fad700326c242e0158bc95a9d7a4f7b5a0
MasonT001 <mltrepagnier@gmail.com>
2022-11-03 12:12:22 -0700

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
