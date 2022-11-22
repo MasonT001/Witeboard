fb4c396c84e15d26a8243d200bfa8ceae77d3e7f
MasonT001 <mltrepagnier@gmail.com>
2022-11-22 11:44:05 -0800

    new login page + routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { ContestsListComponent } from './contests-list/contests-list.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup-login.component';
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: 'privacypolicy', component: PrivacyPolicyComponent },
   { path: 'categories', component: CategoriesComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  { path: 'login', component: LoginComponent}
 
 ];
 
